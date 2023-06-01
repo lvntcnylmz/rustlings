6b4433f872ddc87de9e5193ffb1a618831dd4008
leventcanyilmaz <leventcanyilmaz@proton.me>
2023-06-01 09:48:48 +0300

    solve quiz1 exercises

--- quiz1.rs
+++ quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantitiy: i32) -> i32{
+    if quantitiy > 40 {
+        quantitiy
+    } else {
+        quantitiy * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
