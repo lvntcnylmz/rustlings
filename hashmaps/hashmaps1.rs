e01b0e7f863266ec5fb8926088b80867e334f1c2
leventcanyilmaz <leventcanyilmaz@proton.me>
2023-06-03 12:49:30 +0300

    solve hashmaps exercises

--- hashmaps/hashmaps1.rs
+++ hashmaps/hashmaps1.rs
@@ -8,19 +8,19 @@
 //
 // Make me compile and pass the tests!
 //
-// Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.)
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("orange"), 5);
+    basket.insert(String::from("apple"),4);
 
     basket
 }
