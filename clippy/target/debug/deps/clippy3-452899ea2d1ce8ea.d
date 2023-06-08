cf503a15106ba3a9a73953bae79e36f7ae7003e0
leventcanyilmaz <leventcanyilmaz@proton.me>
2023-06-08 12:44:23 +0300

    solve clippy exercises

---
+++ clippy/target/debug/deps/clippy3-452899ea2d1ce8ea.d
@@ -0,0 +1,8 @@
+/home/can/Documents/github/rustlings/exercises/clippy/target/debug/deps/clippy3-452899ea2d1ce8ea.rmeta: clippy3.rs Cargo.toml
+
+/home/can/Documents/github/rustlings/exercises/clippy/target/debug/deps/clippy3-452899ea2d1ce8ea.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
